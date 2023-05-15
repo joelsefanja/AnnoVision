e5708ef48ad9b17b0b577f4f73eae78227942632
joelsefanja <joelsefanja@gmail.com>
2023-05-15 23:16:19 +0200

    Image viewer gekoppeld aan computer vision yolo model

--- Program/ImageViewer.py
+++ Program/ImageViewer.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtCore import Qt
+import subprocess
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QFileDialog
-
 
 class MyWindow(QWidget):
     def __init__(self):
@@ -21,11 +21,13 @@ class MyWindow(QWidget):
         row1_layout.setContentsMargins(10, 20, 10, 20)
 
         buttons = ['open image', 'predict', 'save as']
+        self.buttons = []
         # Create three buttons and add them to the first row layout
         for b in buttons:
             button = QPushButton(b)
-            button.setStyleSheet("background-color: lightblue; border: none; padding: 20px; font-size: 32px")
+            button.setStyleSheet("background-color: lightblue; padding: 20px; font-size: 32px")
             row1_layout.addWidget(button)
+            self.buttons.append(button)
 
         # Create the second row layout
         row2_layout = QHBoxLayout()
@@ -45,8 +47,13 @@ class MyWindow(QWidget):
         layout.addLayout(row2_layout)
 
         # Connect the first button to open an image
-        button1 = row1_layout.itemAt(0).widget()
-        button1.clicked.connect(self.open_image)
+        self.buttons[0].clicked.connect(self.open_image)
+
+        # Connect the second button to execute a subprocess command
+        self.buttons[1].clicked.connect(self.run_subprocess_command)
+
+        # Variable to store the file path
+        self.file_path = ""
 
     def open_image(self):
         options = QFileDialog.Options()
@@ -55,9 +62,19 @@ class MyWindow(QWidget):
                                                    options=options)
 
         if file_path:
+            self.file_path = file_path  # Store the file path
             pixmap = QPixmap(file_path)
             self.image_label.setPixmap(pixmap.scaled(self.image_label.size(), Qt.KeepAspectRatio))
 
+    def run_subprocess_command(self):
+        if self.file_path:
+            subprocess_command = f"python ../yolov7/detect.py --weights ../yolov7/yolov7.pt --conf 0.25 --img-size 640 --source  {self.file_path}"  # Replace with the actual subprocess command
+            subprocess.run(subprocess_command, shell=True)
+            output_image_path = './runs/detect/exp/aardbei.png'  # Replace with the actual path to the generated image
+            pixmap = QPixmap(output_image_path)
+            self.image_label.setPixmap(pixmap.scaled(self.image_label.size(), Qt.KeepAspectRatio))
+        else:
+            print("No image file selected.")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
